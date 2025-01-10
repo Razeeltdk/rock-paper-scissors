98071a53029d254700d9dd28a152cb5fa81f0054
Razeeltdk <111380890+Razeeltdk@users.noreply.github.com>
2025-01-10 12:58:59 +0530

    Update RPS_game.py

--- rockpapersiccers/RPS_game.py
+++ rockpapersiccers/RPS_game.py
@@ -1,19 +1,19 @@
 def play(player1, player2, num_games, verbose=False):
-    # Simulate a game of Rock, Paper, Scissors
+    
     pass
 
 def quincy(prev_play):
-    # Quincy's strategy
+    # Queen's strategy
     return "R"
 
 def abbey(prev_play):
-    # Abbey's strategy
+    # mark's strategy
     return "P"
 
 def kris(prev_play):
-    # Kris's strategy
+    # don's strategy
     return "S"
 
 def mrugesh(prev_play):
-    # Mrugesh's strategy
+    # marry's strategy
     return "R"
